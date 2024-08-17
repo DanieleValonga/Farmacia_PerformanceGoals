3d3eed30356b7ed4c502408987197611719015d2
Daniele Valonga <143338459+DanieleValonga@users.noreply.github.com>
2024-08-17 03:31:53 -0300

    Add files via upload
    Atualização de footer, home e navbar.

---
+++ src/components/navBar/NavBar.tsx
@@ -0,0 +1,38 @@
+import React, { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../../context/AuthContext';
+import logo from '../../assets/img/logo.png'; // Atualize para o caminho correto
+
+import './Navbar.css'; // Importa o CSS
+
+function Navbar() {
+  const navigate = useNavigate();
+  const { handleLogout } = useContext(AuthContext);
+
+  function logout() {
+    handleLogout();
+    alert('Usuário deslogado com sucesso');
+    navigate('/login');
+  }
+
+  return (
+    <div className='navbar'>
+      <div className='navbar-container'>
+        <div className='navbar-title-container'>
+          <img src={logo} alt="SuperFarma Logo" className='navbar-logo' />
+          <Link to='/home' className='navbar-title'>SuperFarma</Link>
+        </div>
+
+        <div className='navbar-menu'>
+          <Link to='/postagens' className='navbar-item'>Produtos</Link>
+          <Link to='/temas' className='navbar-item'>Categorias</Link>
+          <Link to='/cadastroTema' className='navbar-item'>Cadastrar Produto</Link>
+          <Link to='/perfil' className='navbar-item'>Contato</Link>
+          <Link to='' onClick={logout} className='navbar-item'>Sair</Link>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default Navbar;
